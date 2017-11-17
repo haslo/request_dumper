d20b3306f79c2d44ba9480d3e624b47725c0c733
Guido Gloor <haslo@haslo.ch>
2017-11-17 18:54:24 +0100

    Test successful, now send mail to teambox

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -3,7 +3,7 @@ class RequestMailer < ApplicationMailer
   def request_mail(eui, button)
     @eui = eui
     @button = button
-    mail(from: 'teambox@oxon.ch', to: 'gg@oxon.ch', subject: 'Test Buttons')
+    mail(from: 'teambox@oxon.ch', to: 'teambox@oxon.ch', subject: 'Button wurde gedrückt')
   end
 
 end
